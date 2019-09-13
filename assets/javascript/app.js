595283df36b5bdadc74e038b7358751518e7de7e
Wills Grave <will@keydigitalconsulting.com>
2019-09-13 07:41:31 -0400

    Added a function to render the buttons.  Created a for loop and a var for button.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,21 @@
 // Global Variables
 var topics = ["Brazil", "Germany", "Australia", "France", "Japan", "India", "Egypt", "China", "Cuba", "Russia", "South Africa", "Italy", "Saudi Arabia"]
+
+function renderButtons() {
+  for (var i = 0; i < topics.length; i++) {
+    var buttonName = topics[i];
+
+    var button = `
+      <div class="wrap-buttons">
+        <button
+          class="btn btn-search"
+          data-name="${buttonName}
+        >${buttonName}</button>
+        <button
+          data-name="${buttonName}"
+          class="btn btn-delete"
+        ></button>
+      <div>
+    `;
+  }
+}
